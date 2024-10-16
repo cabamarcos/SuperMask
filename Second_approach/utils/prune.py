e7c3f032194ef09ea9a79b0469c1c0ee046228e9
cabamarcos <marcos.caballero.0203@gmail.com>
2024-10-16 17:14:58 +0200

    Prune weights by setting 70% of the lowest weights to zero

--- Second_approach/utils/prune.py
+++ Second_approach/utils/prune.py
@@ -2,14 +2,16 @@ import torch
 import torch.nn as nn
 
 import numpy as np
+import copy
 
 def prune_weights(net):
     """This function prunes the 70% of the lowest weights in each layer of the network and sets them to zero"""
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    for name, module in net.named_modules():
+    new_net = copy.deepcopy(net)  # Create a copy of the network
+    for name, module in new_net.named_modules():
         if isinstance(module, nn.Linear):
             weights = module.weight.data.cpu().numpy()
             threshold = np.percentile(np.abs(weights), 70)
             weights[np.abs(weights) < threshold] = 0
             module.weight.data = torch.from_numpy(weights).to(device)
-    return net  
+    return new_net
