4371317b2251428ef257c6d4130b6eb7d56988c1
cabamarcos <marcos.caballero.0203@gmail.com>
2024-10-23 20:26:09 +0200

    New helper function

--- Second_approach/utils/count_improvement.py
+++ Second_approach/utils/count_improvement.py
@@ -1,19 +1,18 @@
-def improvements(losses):
-    mejoras = 0
-    peoras = 0
-
-    # Determinar cuántos valores comparar
-    num_valores = min(10, len(losses))
-
-    for i in range(-num_valores, -1):
-        if losses[i] >= losses[i+1]:
-            mejoras += 1
-        elif losses[i] < losses[i+1]:
-            peoras += 1
+def improvements(lista):
+    # Verificar si la lista tiene al menos 10 elementos
+    if len(lista) < 10:
+        raise ValueError("La lista debe contener al menos 10 elementos.")
     
-    if mejoras >= peoras:
-        print("Mejoras")
-        return 0
-    else:
-        print("Peoras")
-        return 1
+    # Inicializar la variable mejoras
+    mejoras = 0
+    
+    # Tomar los últimos 10 elementos de la lista
+    ultimos_10 = lista[-10:]
+    
+    # Comparar los elementos uno a uno
+    for i in range(len(ultimos_10) - 1):
+        if ultimos_10[i + 1] <= ultimos_10[i]:
+            mejoras += 1
+    
+    # Devolver el número de mejoras
+    return mejoras
